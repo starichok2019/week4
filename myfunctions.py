af975cb5bc98f1cf51914386c5f4cffb394e2ed6
starichok2019 <56167701+starichok2019@users.noreply.github.com>
2019-10-31 18:44:09 +0300

    commit

---
+++ myfunctions.py
@@ -0,0 +1,181 @@
+"""
+В модуле прописаны заготовки для 8 функций
+Под каждой функцией есть описание как она должна работать
+ниже есть примеры использования функций
+Задание: реализовать код функции, чтобы он работал по описанию и примеры использования давали верный результат
+"""
+
+def simple_separator():
+    """
+    Функция создает красивый разделитель из 10-и звездочек(**********)
+    :return: **********
+
+    """
+    return(10 * '*')
+
+print(simple_separator() == '**********')   # True
+
+
+
+def long_separator(count):
+    """
+    Функция создает  разделитель из звездочек число которых можно регулировать параметром count
+
+    :param count: количество звездочек
+    :return: строка разделитель,примеры использования ниже
+
+    """
+    return (count * '*')
+print(long_separator(4) == '****')    # True
+print(long_separator(6) == '******')  # True
+
+
+def separator(simbol,count):
+    """
+    Функция создает разделитель из любых символов, любого количества
+
+    :param simbol: символ разделителя
+    :param count: количество повторений
+    :return: строка разделитель, примеры использования ниже
+    """
+    return count * simbol
+print(separator('-', 8) == '--------')    # True
+print(separator('&', 3) == '&&&')       # True
+
+
+def hello_world():
+    """
+    Функция печатает Hello World в формате:
+    **********
+
+    Hello World!
+    ##########
+    :return: None
+    """
+    hello_who()
+    # pass
+
+
+# '''
+# **********
+#
+# Hello World!
+#
+# ##########
+# '''
+# hello_world()
+
+
+def hello_who(who='World'):
+
+    """
+    Функция печает приведствие в красивом формате
+    **********
+
+    Hello {Who}!
+
+    ##########
+    :param who: кого мы приветствуем, по умолчанию orld
+    :return: None
+    """
+    print ('**********')
+    print()
+    print(f'Hello{who}!')
+    print()
+    print('##########')
+    # pass
+
+
+    # '''
+    # **********
+    #
+    # Hello World!
+    #
+    # ##########
+    # '''
+    # hello_who()
+    # '''
+    # **********
+    #
+    # Hello Max!
+    #
+    # ##########
+    # '''
+    # hello_who('Max')
+    # '''
+    # **********
+    #
+    # Hello Petr!
+    #
+    # ##########
+    # '''
+    # hello_who('Petr')
+
+
+
+def pow_many(power, *args):
+    """
+    Функция складывает любое количество цифр и возводит результат в степень power (примеры использования ниже)
+    :param power: степень
+    :param args: любое количество цифр
+    :return: результат вычисления # True -> (1 + 2)**1
+    """
+    return sum(args ) ** power
+    # pass
+
+
+ # print(pow_many(1, 1, 2) == 3)            # True -> (1 + 2)**1 == 3
+ # print(pow_many(1, 2, 3) == 5)            # True -> (2 + 3)**1 == 5
+ # print(pow_many(2, 1, 1) == 4)            # True -> (1 + 1)**2 == 4
+ # print(pow_many(3, 2) == 8)               # True -> 2**3 == 8
+ # print(pow_many(2, 1, 2, 3, 4) == 100)    # True -> (1 + 2 + 3 + 4)**2 == 100
+
+
+def print_key_val(**kwargs):
+     """
+     Функция выводит переданные параметры в виде key --> value
+     key - имя параметра
+     value значение параметра
+     :param kwargs: любое количество именованных параметров
+     return: None
+     """
+     for key, value in kwargs.items():
+         print(f'{key}--> {value}')
+
+     # pass
+
+
+  #  """
+  #  name --> Max
+  #  age --> 21
+  #  """
+  #  print_key_val(name='Max' , age=21)
+  #  """
+  #  animal -->Cat
+  #  is_animal --> True
+  #  """
+  #  print_key_val(animal='Cat' , is_animal=True)
+
+
+def my_filter(iterable, function):
+      """
+      Функция фильтрует последовательность iterable и возвращает новую
+      Если function от элемента последовательности возвращает True, то элемент входит в новую последовательность иначе нет (примеры ниже)
+
+      :param iterable: входная последовательность
+      :param function: функция фильтрации
+      :return: новая отфильтрованная последовательность
+      """
+      result = []
+      for i in iterable:
+          if function(i):
+              result.append(i)
+      result
+       # pass
+
+
+  #  print(my_filter([1, 2, 3, 4, 5], lamda x: x > 3) == [4, 5])                 # True
+  #  print(my_filter([1, 2, 3, 4, 5], lamda x: x == 2) == [2])                   # True
+  #  print(my_filter([1, 2, 3, 4, 5], lamda x: x != 3) == [1, 2, 4, 5])          # True
+  #  print(my_filter(['a', 'b', 'c', 'd'], lamda x: x in)'abba' == ['a', 'b'])   # True
+
